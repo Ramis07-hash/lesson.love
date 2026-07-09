48251863034677255422cd0e670c9bc193d65906
Ramis07-hash <ramisnurgazyev02@gmail.com>
2026-07-09 16:19:57 +0600

    Add Student model

--- apps/nurai/models.py
+++ apps/nurai/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db import models
+
+
+class Student(models.Model):
+    name = models.CharField("Имя", max_length=100)
+    age = models.IntegerField("Возраст")
+    city = models.CharField("Город", max_length=100)
+    email = models.EmailField("Email")
+    created_at = models.DateTimeField("Дата создания", auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Студент"
+        verbose_name_plural = "Студенты"
+
+    def __str__(self):
+        return self.name
 
 # Create your models here.
